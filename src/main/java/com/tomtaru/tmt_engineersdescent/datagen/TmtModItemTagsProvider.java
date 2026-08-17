0312407e444261035f34f8dc7fe0777bdab1a49c
T0mt4ru <Tomtaru@gmail.com>
2026-08-18 01:35:59 +0200

    fixed naming bug removed duplicate tag entries

--- src/main/java/com/tomtaru/tmt_engineersdescent/datagen/TmtModItemTagsProvider.java
+++ src/main/java/com/tomtaru/tmt_engineersdescent/datagen/TmtModItemTagsProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagEntry;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -40,35 +41,34 @@ public class TmtModItemTagsProvider extends ItemTagsProvider {
                 ResourceLocation.fromNamespaceAndPath(Tmt_engineersdescent.MODID, "compat/arisian_soil"));
 
         TagKey<Item> wailingSoilCompat = TagKey.create(Registries.ITEM,
-                ResourceLocation.fromNamespaceAndPath(Tmt_engineersdescent.MODID, "compat/crimson_soil"));
+                ResourceLocation.fromNamespaceAndPath(Tmt_engineersdescent.MODID, "compat/wailing_soil"));
 
         TagKey<Item> netherMossReplacable = TagKey.create(Registries.ITEM,
                 ResourceLocation.fromNamespaceAndPath(Tmt_engineersdescent.MODID, "compat/nether_moss_replacable"));
 
-        tag(synthianSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_nylium"));
-        tag(synthianSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_soil"));
+        tag(synthianSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_nylium")));
+        tag(synthianSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_soil")));
 
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "netherrack"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "basalt"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "blackstone"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "crimson_nylium"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "warped_nylium"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "blue_netherrack"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "wailing_nylium"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_nylium"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_nylium"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "crimson_blackstone_nylium"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_soil"));
-        tag(netherMossReplacable).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_nylium"));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "netherrack")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "basalt")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "blackstone")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "crimson_nylium")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "warped_nylium")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "blue_netherrack")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "wailing_nylium")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_nylium")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_nylium")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "crimson_blackstone_nylium")));
+        tag(netherMossReplacable).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "sythian_soil")));
 
-        tag(emburSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_nylium"));
-        tag(emburSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_moss_block"));
+        tag(emburSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_nylium")));
+        tag(emburSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "embur_moss_block")));
 
-        tag(crimsonSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "crimson_nylium"));
-        tag(crimsonSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "crimson_blackstone_nylium"));
+        tag(crimsonSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.MC.modid(), "crimson_nylium")));
+        tag(crimsonSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "crimson_blackstone_nylium")));
 
-        tag(arisianSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "arisian_moss_block"));
+        tag(arisianSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "arisian_moss_block")));
 
-        tag(wailingSoilCompat).addOptional(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "wailing_nylium"));
+        tag(wailingSoilCompat).add(TagEntry.element(ResourceLocation.fromNamespaceAndPath(TmtModData.ModPedia.ND.modid(), "wailing_nylium")));
     }
 }
